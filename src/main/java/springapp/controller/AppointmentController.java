29445c14662f61b1805fa91c17717be5e97e5ad0
stubclan <aryal.sujan8@gmail.com>
2019-03-22 16:30:30 -0500

    Added on Readme

--- src/main/java/springapp/controller/AppointmentController.java
+++ src/main/java/springapp/controller/AppointmentController.java
@@ -1,5 +1,8 @@
 package springapp.controller;
 
+
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +36,7 @@ public class AppointmentController {
 	AppointmentService appointmentService;
 	
 	@GetMapping
-	public String get() {
+	public String get(Model model) {
         return "appointments/listAppointments";
    }
 
