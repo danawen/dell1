b1e984fad9ecb95a66cf67acd8d62ec90550bd5f
Ryan_McClure <ryan_mcclure@dell.com>
2019-03-29 16:17:38 -0500

    Added Reason as a drop down enum for pet appointment
    Added Reason as a drop down enum for pet appointment

--- src/main/java/springapp/domain/Appointment.java
+++ src/main/java/springapp/domain/Appointment.java
@@ -1,35 +1,70 @@
 package springapp.domain;
+import java.sql.Timestamp;
 
-public class Client {
+public class Appointment {
 	private final Integer id;
-	private final String name;
-	private final String phoneNumber;
-	private final String address;
+	private final Integer petId;
+	private final Integer clientId;
+	private Reason reason;
+	private Timestamp time;
+	private Integer duration;
+	private String comments;
 	
-
-	public Client(Integer id, String name, String phoneNumber, String address) {
-		this.id = id;
-		this.name = name;
-		this.phoneNumber = phoneNumber;
-		this.address = address;
+	public Appointment(Integer myId, Integer myPetId, Integer myClientId, Reason myReason, Timestamp myApptTime, Integer myDuration, String myComments){
+		
+		this.id = myId;
+		this.petId = myPetId;
+		this.clientId = myClientId;		
+		this.reason = myReason;
+		this.time = new Timestamp(System.currentTimeMillis());
+		this.duration = myDuration;
+		this.comments = myComments;
+		
 	}
-
 	
-	public String getName() {
-		return name;
+	public Reason getReason() {
+		return reason;
 	}
 
-	public String getPhoneNumber() {
-		return phoneNumber;
+	public void setReason(Reason apptReason) {
+		this.reason = apptReason;
 	}
 
-	public String getAddress() {
-		return address;
+	public Timestamp getTime() {
+		return time;
+	}
+
+	public void setTime(Timestamp time) {
+		this.time = time;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	public void setDuration(Integer duration) {
+		this.duration = duration;
 	}
 
 	public Integer getId() {
 		return id;
 	}
+
+	public Integer getPetId() {
+		return petId;
+	}
+
+	public Integer getClientId() {
+		return clientId;
+	}
+
+	public Integer getDuration() {
+		// TODO Auto-generated method stub
+		return duration;
+	}	
 	
 	
 }
